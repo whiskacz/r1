89d4a582470eb39f11a8e2a39594e959a8edc8bf
whiskacz <pawelblasiak1985@gmail.com>
2023-07-05 20:47:39 +0200

    React Practice - R1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function FirstElement(){
+    const [imie, setImie] = useState("Żyrafa")
+    const [data, setData] = useState("01-01-1985")
+    
+    const handleImieChange = (e) => {
+        setImie(e.target.value)
+    }
+    const handleDataChange = (e) => {
+        setData(e.target.value)
+    }
+
+    return(
+    <>
+    <div>Twoję imię to {!imie ? ">>> tutaj wstaw imię<<<" : imie}</div>
+    <div>Twoja data urodzenia to {!data ? ">>> tutaj się pojawi Twoja data <<<" : data}</div>
+    <input type="text" onChange={handleImieChange} value={imie}/>
+    <input type="date" onChange={handleDataChange}/>
+    </>
+)}
+
+
+const el = document.getElementById("root")
+const root = ReactDOM.createRoot(el)
+root.render(<FirstElement />)
